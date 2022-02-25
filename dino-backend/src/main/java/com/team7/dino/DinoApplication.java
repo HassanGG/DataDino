130741fc6c80175f7488e37a4dbb175e3f442d96
HassanGG <hassan.g.ghnewa@gmail.com>
2022-02-25 15:31:20 +0000

    Initial backend

---
+++ dino-backend/src/main/java/com/team7/dino/DinoApplication.java
@@ -0,0 +1,13 @@
+package com.team7.dino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoApplication.class, args);
+	}
+
+}
---
+++ dino-backend/src/test/java/com/team7/dino/DinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.dino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
