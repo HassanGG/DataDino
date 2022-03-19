c1682ce00291c883ac825688b9b878e9798ce0b3
HassanGG <hassan.g.ghnewa@gmail.com>
2022-03-19 10:55:09 +0000

    fixed bug with admin creation

--- dino-backend/src/main/java/com/team7/dino/repository/UserRepository.java
+++ dino-backend/src/main/java/com/team7/dino/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> getUserByEmailAndPassword(String email, String password);
+    Optional<User> getUserByEmail(String email);
 }
